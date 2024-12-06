512fb7c277c21bc4e6043ff4e1c4f7bb3b326328
ardy-jaya <ardy.henjaya@gmail.com>
2024-12-06 12:20:05 +0700

    feat: not auth navbar
    not auth navbar include sign in and sign up but exclude profile and notification bar

--- src/app/component/navbar_not_auth.tsx
+++ src/app/component/navbar_not_auth.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Menu, MenuButton, MenuItem, MenuItems } from "@headlessui/react";
 import { ChevronDownIcon } from "@heroicons/react/24/solid";
 
-const Navbar: React.FC = () => {
+const Navbar_not_auth: React.FC = () => {
   return (
     <nav className="bg-white border-gray-200 dark:bg-gray-900 dark:border-gray-700">
       <div className="flex-auto p-4">
@@ -123,59 +123,29 @@ const Navbar: React.FC = () => {
               </button>
             </form>
           </li>
+          <div
+          className="flex">          
           <li>
             <button
               type="button"
-              className="relative inline-flex items-center p-2 text-sm text-gray-500 rounded-lg hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+              className="inline-flex items-center px-4 py-2 text-sm font-medium text-white bg-blue-600 rounded-lg hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500"
             >
-              <svg
-                className="w-6 h-6"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V4a2 2 0 10-4 0v1.341C7.67 7.165 6 9.388 6 12v2.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
-                />
-              </svg>
-              <span className="sr-only">Notifications</span>
-              <span className="absolute top-0 right-0 inline-flex items-center justify-center w-3 h-3 text-xs font-bold text-white bg-red-500 rounded-full">
-                10
-              </span>
+              Sign In
             </button>
           </li>
-          <li className="justify-items-end">
+          <li>
             <button
               type="button"
-              className="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+              className="inline-flex items-center px-4 py-2 text-sm font-medium text-white bg-green-600 rounded-lg hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-500"
             >
-              <svg
-                className="w-6 h-6"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M5.121 17.804A9.004 9.004 0 0112 15c2.21 0 4.21.896 5.879 2.804M15 11a3 3 0 11-6 0 3 3 0 016 0z"
-                />
-              </svg>
-              <span className="sr-only">Profile</span>
+              Sign Up
             </button>
           </li>
+          </div>
         </ul>
       </div>
     </nav>
   );
 };
 
-export default Navbar;
+export default Navbar_not_auth;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,14 @@ import React from "react";
 import Image from "next/image";
 import Homes from "./home";
 import Navbar from "./component/navbar";
+import Navbar_not_auth from "./component/navbar_not_auth";
 
 export default function Home() {
   return (
     <div>
       <Navbar />
+      <Navbar_not_auth />
+      
       <Homes />
       <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
         <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
